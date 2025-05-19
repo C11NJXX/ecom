1789f9b5e6807780f5380ed2c05b54c50ff4bfe4
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-05-19 16:07:13 +0800

    products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,16 @@
 import { stripe } from "@/lib/stripe";
+import ProductList from "@/components/ProductList";
 
 export default async function ProductPage() {
   const products = await stripe.products.list({
     expand: ["data.default_price"],
   });
-  return <div>products page</div>;
+  return (
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+        All Products
+      </h1>
+      <ProductList products={products.data} />
+    </div>
+  );
 }
