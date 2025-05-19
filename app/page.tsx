6eaef67f57f0984d8fb7d2037cbd2fd84b216b32
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-05-19 11:20:22 +0800

    first section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -15,7 +17,16 @@ export default async function Home() {
           <div>
             <h2>Welcome to My Ecommerce</h2>
             <p>Discover the latest products at the best prices</p>
+            <Button asChild variant="default">
+              <Link href="/products">Browse All Products</Link>
+            </Button>
           </div>
+          <Image
+            alt="Banner Image"
+            width={450}
+            height={450}
+            src={products.data[0].images[0]}
+          />
         </div>
       </section>
     </div>
